c37ecbe5c7251b7c473c77f6ee311207102b4469
Vovchegg <vovchegg94@gmail.com>
2023-04-24 22:22:51 +0300

    Correct Calculator class

--- src/Calculator/Calculator.java
+++ src/Calculator/Calculator.java
@@ -7,7 +7,17 @@ public class Calculator {
     BinaryOperator<Integer> plus = (x, y) -> x + y;
     BinaryOperator<Integer> minus = (x, y) -> x - y;
     BinaryOperator<Integer> multiply = (x, y) -> x * y;
-    BinaryOperator<Integer> devide = (x, y) -> x / y;
+    BinaryOperator<Integer> devide = (x, y) -> {
+        try {
+            if (y == 0)
+                throw new ArithmeticException("Ошибка! На 0 делить нельзя.");
+            return x / y;
+
+        } catch (ArithmeticException e) {
+            System.out.println("Ошибка! На 0 делить нельзя.");
+            return 0;
+        }
+    };
     UnaryOperator<Integer> pow = x -> x * x;
     UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
     Predicate<Integer> isPositive = x -> x > 0;
--- src/Calculator/Main.java
+++ src/Calculator/Main.java
@@ -6,12 +6,9 @@ public class Main {
 
         int a = calc.plus.apply(1, 2);
         int b = calc.minus.apply(1, 1);
-        try {
-            int c = calc.devide.apply(a, b);
-            calc.println.accept(c);
-        } catch (Exception e) {
-            System.out.println("Деление на ноль!");
-        }
+        int c = calc.devide.apply(a, b);
+        calc.println.accept(c);
+
         int d = calc.multiply.apply(a, b);
         calc.println.accept(d);
 
